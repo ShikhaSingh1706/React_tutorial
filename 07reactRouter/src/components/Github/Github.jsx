2e2116504c95484bf7f4beb42ffae45ba944c21e
Limber <it@limberenergy.com>
2025-02-27 16:44:46 +0530

    context api data

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -12,7 +12,8 @@ function Github() {
     //     setData(data)
     //  })
     // }, [])
-    
+
+     
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github following: {data.following}
     <img src={data.avatar_url} alt="Git picture" width={300} />
---
+++ 08_minicontext/src/App.jsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+
+
+function App() {
+
+  return (
+    < UserContextProvider>
+      <h1 >React with Context API</h1>
+      <Login />
+     <Profile />
+      </UserContextProvider>
+  ) 
+}
+
+export default App
